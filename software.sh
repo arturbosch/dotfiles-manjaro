d2eb76879f24a0e8aa68a0caf17082b010bc819b
Artur Bosch <arturbosch@gmx.de>
2019-01-06 16:43:59 +0100

    Update dotfiles to latest arch setup

--- software.sh
+++ software.sh
@@ -1,14 +1,17 @@
-echo "Installing sdkman(zip, unzip)"
-inst unzip
-inst zip
-curl -s "https://get.sdkman.io" | bash
+echo "Installing software"
+sudo pacman -S lsof git youtube-dl htop udisksctl udisks2 wget fuse2 gksu kitty exa ripgrep fd neofetch tokei nnn fzf zjump pywal udiskie ntfs-3g lxappearance lxrandr
+echo "Installing archive extract tools"
+sudo pacman -S xarchiver p7zip zip unzip unrar
 
+echo "Installing sdkman"
+curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk i java
 sdk i gradle
 sdk i maven
 sdk i groovy
 
-echo "Installing git"
-inst git
-inst youtube-dl
+echo "From Yaourt"
+yaourt light-git
+yaourt gnome-breeze-git
+yaourt arc-icon-theme-git
