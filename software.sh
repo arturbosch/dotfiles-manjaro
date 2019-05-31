1057c224333efdfe72f51d46cc9f739c5d0843ca
Artur Bosch <arturbosch@gmx.de>
2019-05-31 12:40:01 +0200

    Add alsa utils for moc

--- software.sh
+++ software.sh
@@ -1,7 +1,7 @@
-echo "Installing software"
-sudo pacman -S lsof git youtube-dl htop udisksctl udisks2 wget fuse2 gksu kitty exa ripgrep fd neofetch tokei nnn fzf zjump pywal udiskie ntfs-3g lxappearance lxrandr tig
 echo "Installing archive extract tools"
 sudo pacman -S xarchiver p7zip zip unzip unrar
+echo "Installing software"
+sudo pacman -S lsof git youtube-dl htop udisksctl udisks2 wget fuse2 gksu alacritty exa ripgrep fd neofetch tokei nnn fzf zjump pywal udiskie ntfs-3g lxappearance lxrandr tig xdg-utils alsa-utils moc
 
 echo "Installing sdkman"
 curl -s "https://get.sdkman.io" | bash
