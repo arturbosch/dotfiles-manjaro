b01122eb1be43ceeb9cafc2b5f788f7729960814
Artur Bosch <arturbosch@gmx.de>
2019-06-12 17:34:01 +0200

    mkdirs and links

---
+++ links.sh
@@ -0,0 +1,6 @@
+#!/bin/env sh
+ln -s ~/Repos/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/Repos/dotfiles/fish/abosch.fish ~/.config/fish/conf.d/conf.fish
+ln -s ~/Repos/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -s ~/Repos/dotfiles/ranger/commands.py ~/.config/ranger/commands.py
+ln -s ~/Repos/dotfiles/rofi/config ~/.config/rofi/config
---
+++ mkdirs.sh
@@ -0,0 +1,12 @@
+#!/bin/env sh
+mkdir ~/.config/nvim
+mkdir ~/.config/i3
+mkdir ~/.config/rofi
+mkdir ~/.config/ranger
+mkdir -p ~/.config/fish/conf.d
+mkdir ~/bin
+mkdir ~/Repos
+mkdir ~/Scripts
+mkdir ~/Tools
+mkdir ~/Files
+
