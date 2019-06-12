c3dfdef487c65e1d2f44e30bef2dddb97325bce8
Artur Bosch <arturbosch@gmx.de>
2019-06-12 17:35:31 +0200

    add i3 link

--- links.sh
+++ links.sh
@@ -1,4 +1,6 @@
 #!/bin/env sh
+ln -s ~/Repos/dotfiles/i3/config ~/.config/i3/config
+ln -s ~/Repos/dotfiles/i3/status.toml ~/.config/i3/status.toml
 ln -s ~/Repos/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/Repos/dotfiles/fish/abosch.fish ~/.config/fish/conf.d/conf.fish
 ln -s ~/Repos/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
