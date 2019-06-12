ae622a49a0788e99d6ee4870bffc162dca26f5dc
Artur Bosch <arturbosch@gmx.de>
2019-06-12 17:38:02 +0200

    move i3 config file first

--- links.sh
+++ links.sh
@@ -1,4 +1,5 @@
 #!/bin/env sh
+mv ~/.config/i3/config ~/.config/i3/config.bak
 ln -s ~/Repos/dotfiles/i3/config ~/.config/i3/config
 ln -s ~/Repos/dotfiles/i3/status.toml ~/.config/i3/status.toml
 ln -s ~/Repos/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
