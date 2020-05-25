439ba166ec5d4c433d03d0c315b22118976adfa2
Artur Bosch <arturbosch@gmx.de>
2020-05-25 23:31:53 +0200

    Add gutenprint and alacritty

--- regolith/install.sh
+++ regolith/install.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 # Regolith Linux after install script
 
-# system
-sudo apt install htop powertop tlp curl fish git fzf ripgrep tig meld tk tcl tldr tldr-py autojump fd-find suckless-tools asciinema scrot thunar geary neovim neovim-qt
+# additional software
+sudo apt install htop powertop tlp \
+    printer-driver-gutenprint \
+    curl fish git fzf ripgrep tig meld tk tcl tldr tldr-py autojump \
+    fd-find suckless-tools asciinema scrot \
+    thunar geary \
+    neovim neovim-qt \
+    nodejs yarn \
+    hugo jekyll
 
+# fish shell package manager
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
-# dev
+# terminal emulator
+sudo add-apt-repository ppa:mmstick76/alacritty
+sudo apt install alacritty
 
-## js
-sudo apt install nodejs yarn
-
-## jdk
+# JVM development
 # curl -s "https://get.sdkman.io" | bash
 # source "$HOME/.sdkman/bin/sdkman-init.sh"
 # sdk i gradle
